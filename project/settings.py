6e9c4665e52518181d2fd10560316598708ae154
Rohan Baddi <ronbaddi004@gmail.com>
2020-06-09 20:05:30 +0530

    Added dj-database-url

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
